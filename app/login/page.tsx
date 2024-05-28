cbc580e0340a45ac2ab2468415b036a3090f3098
rakshanshgandhi <rakshanshgandhi@gmail.com>
2024-05-28 18:09:04 +0530

    Header added for log in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -17,7 +18,7 @@ export default function LoginPage() {
       setButtonDisabled(true);
     }
   }, [user]);
-  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const onLogin = async () => {
     try {
       const response = await axios.post("/api/login", user);
@@ -27,37 +28,69 @@ export default function LoginPage() {
       console.log("SignIn failed");
     }
   };
+  const onSignup = () => {
+    router.push("/signup");
+  }
   return (
     <>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          required
-        />
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="p-6 rounded-lg shadow-lg border-2 w-full max-w-sm bg-white">
+          <h1 className="text-center font-bold text-2xl mb-5">
+            Sign In
+          </h1>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            required
+          />
+        </div>
+        <button onClick={onLogin} disabled={buttonDisabled}
+        className="w-full  py-2 rounded-lg mb-4 bg-blue-600 text-white hover:bg-blue-700  transition duration-200">
+          Sign In
+        </button>
+        <hr/>
+        <div className="mt-4">
+            <button
+              onClick={() => signIn('google',{
+                callbackUrl : '/home'
+              })}
+              className="w-full bg-gray-100 text-black py-2 rounded-lg mb-2 flex items-center justify-center"
+            >
+              <img className="w-6 h-6 mr-2" src='/google.svg' alt='google logo' />
+              Sign in with Google
+            </button>
+            <p className="text-center mt-4">Don't have an account?{' '}
+            <button
+              type="button"
+              onClick={onSignup}
+              className="text-blue-600 hover:underline"
+            >
+              Register here
+            </button>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-gray-700">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          required
-        />
-      </div>
-      <button onClick={onLogin}>
-        {buttonDisabled ? "No Sign In" : "Sign In"}
-      </button>
     </>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { signIn } from "next-auth/react";
 
 export default function SignupPage() {
   const router = useRouter();
@@ -11,6 +12,9 @@ export default function SignupPage() {
     password: "",
     username: "",
   });
+  const onSignIn = () => {
+    router.push("/login");
+  };
   useEffect(() => {
     if (
       user.email.length > 0 &&
@@ -34,49 +38,86 @@ export default function SignupPage() {
   };
   return (
     <>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-gray-700">
-          Name
-        </label>
-        <input
-          type="text"
-          id="username"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-          className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          required
-        />
-      </div>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="p-6 shadow-md border-2 rounded-lg bg-white w-full max-w-sm">
+          <h1 className="text-center font-bold text-2xl mb-5">Sign Up</h1>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700">
+              Name
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+              required
+            />
+          </div>
 
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          required
-        />
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+              required
+            />
+          </div>
+          <button
+            onClick={onSignup}
+            disabled={buttonDisabled}
+            className="w-full  py-2 rounded-lg mb-4 bg-blue-600 text-white hover:bg-blue-700  transition duration-200"
+          >
+            Sign up
+          </button>
+          <hr />
+          <div className="mt-4">
+            <button
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "/home",
+                })
+              }
+              className="w-full bg-gray-100 text-black py-2 rounded-lg mb-2 flex items-center justify-center"
+            >
+              <img
+                className="w-6 h-6 mr-2"
+                src="/google.svg"
+                alt="google logo"
+              />
+              Sign up with Google
+            </button>
+            <p className="text-center mt-4">
+              Have account already?{" "}
+              <button
+                type="button"
+                onClick={onSignIn}
+                className="text-blue-600 hover:underline"
+              >
+                Sign In
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-gray-700">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          className="w-full px-4 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          required
-        />
-      </div>
-      <button onClick={onSignup}>
-        {buttonDisabled ? "No Sign up" : "Sign up"}
-      </button>
     </>
   );
 }
