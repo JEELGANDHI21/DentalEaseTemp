71e7cee21bd908439917331cf09f15df516cc82f
Jeel Gandhi <jeelgandhi4585@gmail.com>
2024-05-31 13:59:42 +0530

    Clerk Role Based Access Added
    Roles are : Admin, Doctor and Patient

---
+++ types/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    primaryEmailAddressId: string;
+    emailAddresses: { id: string; emailAddress: string }[];
+    publicMetadata: {
+      role: string;
+    };
+  }
+  
